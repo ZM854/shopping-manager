7df038dfe696e071b7658a4843ca796ec2dcf1f5
ZM854 <zaharmedvedev2006@mail.ru>
2026-07-25 07:41:21 +0300

    fix(backend): add products table to db-reset script

--- backend/scripts/clear_users.sql
+++ backend/scripts/clear_users.sql
@@ -1,5 +1,7 @@
 DELETE FROM refresh_tokens;
+DELETE FROM products;
 DELETE FROM users;
 
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 ALTER SEQUENCE refresh_tokens_id_seq RESTART WITH 1;
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
